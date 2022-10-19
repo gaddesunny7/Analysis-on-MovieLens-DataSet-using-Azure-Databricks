a662200992de4365cdbe21d9ec2fb0520b578803
SUNNY NAGESH GADDE <gaddesunny7@gmail.com>
2022-10-19 16:45:12 +0200

    Update authorization.py

--- code/authorization.py
+++ code/authorization.py
@@ -5,22 +5,22 @@ spark
 
 #Connect To Azure DataLakeStorage
 spark.conf.set(
-            "fs.azure.account.key.sadevdatalakeaz.dfs.core.windows.net",
-            "cYaqBCIC7Njfa54+EpF5o0MRIm4ePv9pQT6fAKbSdgnNl25z7LQS7M4xnXI0t6zpCMl2EuNxhl16+AStgcF85g=="
+            "fs.azure.account.key.<storage-account-name>.dfs.core.windows.net",
+            "<Generate Token>"
             )
 
 # COMMAND ----------
 
 #Access the files in the container through the Data Lake Storage Account
-dbutils.fs.ls("abfss://sacondatalakedevaz@sadevdatalakeaz.dfs.core.windows.net/")
+dbutils.fs.ls("abfss://<storage-container-name>@<storage-account-name>.dfs.core.windows.net/")
 
 # COMMAND ----------
 
 #Commenting it out because it will be used in other notebook
 #dbutils.fs.mount(
-#source = "wasbs://sacondatalakedevaz@sadevdatalakeaz.blob.core.windows.net",
-#mount_point = "/mnt/sacondatalakedevaz",
-#extra_configs = {"fs.azure.sas.sacondatalakedevaz.sadevdatalakeaz.blob.core.windows.net":"sp=r&st=2022-10-16T15:30:05Z&se=2022-10-16T23:30:05Z&spr=https&sv=2021-06-08&sr=c&sig=4JSrCd2KqxSuudFMqgMl41RXRDHsUgZNN3GZeUv1HEQ%3D"})
+#source = "wasbs://<storage-container-name>@<storage-account-name>.blob.core.windows.net",
+#mount_point = "/mnt/<storage-container-name>",
+#extra_configs = {"fs.azure.sas.<storage-container-name>.<storage-account-name>.blob.core.windows.net":"<Generate Token>"})
 
 # COMMAND ----------
 
@@ -30,4 +30,4 @@ dbutils.fs.ls('/mnt/')
 # COMMAND ----------
 
 #Unmount a file in the DBFS
-#dbutils.fs.unmount("/mnt/azdwsmovielens/")
+#dbutils.fs.unmount("/mnt/<storage-container-name>/")
